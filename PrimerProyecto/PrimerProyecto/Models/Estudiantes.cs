bb0a9c904f39616ce0c26e380046f468ca4f598f
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-08-17 11:51:47 -0400

    Realice modelo estudiantes

---
+++ PrimerProyecto/PrimerProyecto/Models/Estudiantes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerProyecto.Models;
+
+public class Estudiantes
+{
+    [Key]
+    public int EstudianteId { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public int? Matricula { get; set; }
+
+    public string? Carrera { get; set; }
+}
