33849d7e2c48e70e651803dc44640a36496e5f9d
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2024-08-21 15:18:07 -0400

    Se agregaron las validaciones

--- PrimerProyecto/PrimerProyecto/Models/Estudiantes.cs
+++ PrimerProyecto/PrimerProyecto/Models/Estudiantes.cs
@@ -7,9 +7,14 @@ public class Estudiantes
     [Key]
     public int EstudianteId { get; set; }
 
+    [Required(ErrorMessage ="Nombre obligatorio")]
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Nombre { get; set; }
 
+    [Required(ErrorMessage = "Matricula obligatoria")]
     public int? Matricula { get; set; }
 
+    [Required(ErrorMessage = "Carrera obligatorio")]
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras")]
     public string? Carrera { get; set; }
 }
